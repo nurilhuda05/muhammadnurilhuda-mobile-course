3b77c160ccc380f9798ace2bb6ad682314c5adae
Muhammad Nuril Huda <mnurilhuda2005@gmail.com>
2026-08-26 20:39:13 +0700

    menambahkan nim dan kelas

--- 01-week-1-mobile-development-ecosystem-flutter-refresh/my_first_app/lib/main.dart
+++ 01-week-1-mobile-development-ecosystem-flutter-refresh/my_first_app/lib/main.dart
@@ -12,9 +12,11 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: const Text('Profil Mahasiswa')),
         body: const Center(
           child: Column(mainAxisSize: MainAxisSize.min, children: [
-            Icon(Icons.school, size: 72),
+            Icon(Icons.person, size: 72),
             SizedBox(height: 16),
-            Text('Muhammad Nuril Huda', style: TextStyle(fontSize: 24)),
+            Text('Muhammad Nuril Huda', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold )),
+            Text('Teknik Informatika — 3G', style: TextStyle(fontSize: 20)),
+            Text('244107020004', style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic)),
             Text('Pemrograman Mobile — Minggu 1'),
           ]),
         ),
