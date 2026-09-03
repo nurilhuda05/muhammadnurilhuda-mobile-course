daa1efab8ab76c5dceb12a8dcf8c3e36ddaba753
Muhammad Nuril Huda <mnurilhuda2005@gmail.com>
2026-09-03 18:52:52 +0700

    Melakukan Testing Dasar Menggunakan Flutter Test

--- 02-week-2-declarative-ui-responsive-design/responsive_dashboard/test/widget_test.dart
+++ 02-week-2-declarative-ui-responsive-design/responsive_dashboard/test/widget_test.dart
@@ -1,30 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:responsive_dashboard/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const  DashboardApp());
+  testWidgets('Dashboard satu kolom di layar sempit', (tester) async {
+    tester.view.physicalSize = const Size(400, 800);
+    tester.view.devicePixelRatio = 1.0;
+    addTearDown(tester.view.reset);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(const DashboardApp());
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    final gridView = tester.widget<GridView>(find.byType(GridView));
+    final gridDelegate =
+        gridView.gridDelegate as SliverGridDelegateWithFixedCrossAxisCount;
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(gridDelegate.crossAxisCount, 1);
+  });
+
+  testWidgets('Dashboard dua kolom di layar lebar', (tester) async {
+    tester.view.physicalSize = const Size(1200, 800);
+    tester.view.devicePixelRatio = 1.0;
+    addTearDown(tester.view.reset);
+
+    await tester.pumpWidget(const DashboardApp());
+
+    final gridView = tester.widget<GridView>(find.byType(GridView));
+    final gridDelegate =
+        gridView.gridDelegate as SliverGridDelegateWithFixedCrossAxisCount;
+
+    expect(gridDelegate.crossAxisCount, 2);
   });
 }
