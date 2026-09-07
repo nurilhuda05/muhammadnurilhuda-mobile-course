6d3aa95c879b51d11fedc306e1730a0a87e06084
Muhammad Nuril Huda <mnurilhuda2005@gmail.com>
2026-09-07 23:24:59 +0700

    Praktikum 2 pertemuan 3

--- 03-week-3-navigation-state-management/week3_navigation/lib/main.dart
+++ 03-week-3-navigation-state-management/week3_navigation/lib/main.dart
@@ -1,36 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'pages/detail_page.dart';
-import 'pages/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'pages/todo_page.dart';
 
-void main() => runApp(const MyApp());
-
-final _router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomePage(),
-      routes: [
-        GoRoute(
-          path: 'detail/:id',
-          builder: (context, state) => DetailPage(
-            id: state.pathParameters['id']!,
-          ),
-        ),
-      ],
-    ),
-  ],
-);
+void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Week 3 - Navigation',
-      routerConfig: _router,
-      theme: ThemeData(colorSchemeSeed: Colors.indigo, useMaterial3: true),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Week 3 - ToDo',
+        theme: ThemeData(colorSchemeSeed: Colors.teal, useMaterial3: true),
+        home: const TodoPage(),
+      );
 }
